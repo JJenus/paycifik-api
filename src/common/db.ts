86e594f4840a460491220b986dc9b3f0cc80ec58
jjenus <biz.jenus@gmail.com>
2024-02-28 17:01:33 +0100

    dsf

--- src/common/db.ts
+++ src/common/db.ts
@@ -1,15 +1,15 @@
 import { Sequelize } from "sequelize";
 
-let db = process.env.DB_NAME || "zipay_db";
+let db = process.env.DB_NAME || "paycifik_db";
 let user = process.env.DB_USER || "root";
 let pass = process.env.DB_PASSWORD || "";
 let host = process.env.DB_HOST || "localhost";
 
 if (process.env.NODE_ENV === "production") {
-	db = process.env.DB_NAME || "fibre_zipay_db";
-	user = process.env.DB_USER || "fibre";
-	pass = process.env.DB_PASSWORD || "5olidpayout";
-	host = process.env.DB_HOST || "mysql-fibre.alwaysdata.net";
+	db = process.env.DB_NAME || "paycifik_db";
+	user = process.env.DB_USER || "paycifik";
+	pass = process.env.DB_PASSWORD || "@Asdf1234T";
+	host = process.env.DB_HOST || "mysql-paycifik.alwaysdata.net";
 }
 
 let sequelize: Sequelize = new Sequelize(db, user, pass, {
